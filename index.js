391d9b0f93963bef823ffe9b13eb9007acac83d9
Rascafr <rascafr@gmail.com>
2019-03-18 15:17:55 +0100

    +  module index added

---
+++ index.js
@@ -0,0 +1,7 @@
+// Node module: loopback-secure-storage
+// François Leparoux - 2019
+// File licenced under MIT license
+'use strict';
+
+var SecureStorage = require('./lib/secure-storage');
+module.exports = SecureStorage;
